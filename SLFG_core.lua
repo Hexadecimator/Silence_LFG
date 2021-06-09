6774abe87fe0ea3596921c2e52afab87314ac988
Logan <logan.scholz@gmail.com>
2021-06-08 21:51:08 -0700

    fixed locking and slash cmds
    fixed locking and slash cmds

--- SLFG_core.lua
+++ SLFG_core.lua
@@ -17,7 +17,7 @@ end
 --[[
 
 TODO:
-1. make frame
+1. make frame:
     - very small
     - will go under minimap (default)
     - will be moveable
@@ -28,48 +28,22 @@ TODO:
     - Maybe a checkbox instead of a symbol? Interactable
       control which is also visual feedback for addon status
 
+2. Save data
+    - window x,y position and restore upon reload
+        - x,y info will be saved when user locks window
+    - state of LFG (was it disabled/enabled when you logged off)
+
+WISHLIST:
+1. AddOn options page with font/button sizing/button texturing options
+
 --]]
 
 -- **********************************************
 -- ** START MAIN FRAME DEFINITION SECTION *******
 -- **********************************************
 
---local UIMain = CreateFrame("Frame", "SLFG_MainFrame", UIParent, "SLFGFrameTemplate");
---[[
-local UIMain = CreateFrame("Frame", "SLFG_MainFrame", UIParent, "BaseBasicFrameTemplate");
-UIMain:SetSize(100, 40); -- width, height
-UIMain:SetPoint("CENTER", UIParent, "CENTER"); -- point, relativeFrame, relativePoint, xOffset, yOffset
-
-UIMain.title = UIMain:CreateFontString(nil, "OVERLAY");
-UIMain.title:SetFontObject("GameFontHighlight");
-UIMain.title:SetPoint("CENTER", UIMain.Bg, "CENTER", -15, 9);
-UIMain.title:SetText("LFG--    ");
-
-UIMain.cb1 = CreateFrame("CheckButton", "slfgCB1", UIMain, "ChatConfigCheckButtonTemplate");
-UIMain.cb1:SetPoint("LEFT", UIMain.TitleBg, "CENTER", 0, 9);
-UIMain.cb1.tooltip = "show/hide LFG channel";
-local slfgCBState = false;
-UIMain.cb1:SetScript("OnClick", 
-                        function(self)
-                            -- todo
-                            slfgCBState = not slfgCBState;
-                            if (slfgCBState) then
-                                --local t, name = JoinChannelByName("LookingForGroup");
-                                --JoinChannelByName("LookingForGroup");
-                                local channel_type, channel_name = JoinChannelByName("LookingForGroup");
-                                ChatFrame_AddChannel(DEFAULT_CHAT_FRAME, "LookingForGroup");
-                                print("JOINED LFG");
-                            else
-                                LeaveChannelByName("LookingForGroup");
-                                --print("LEFT LFG");
-                            end
-                        end)
-
---]]
-
-
-
 local slfgBTNState = false;
+
 UItogBtn = CreateFrame("Button", nil, UIMain, "GameMenuButtonTemplate");
 UItogBtn:SetPoint("CENTER", UIMain, "TOP", 0, -80);
 UItogBtn:SetSize(45, 30);
@@ -84,10 +58,20 @@ UItogBtn:SetScript("OnClick",
                                         --JoinChannelByName("LookingForGroup");
                                         local channel_type, channel_name = JoinChannelByName("LookingForGroup");
                                         ChatFrame_AddChannel(DEFAULT_CHAT_FRAME, "LookingForGroup");
+                                        print("W-H-L-R");
+                                        print(UItogBtn:GetWidth());
+                                        print(UItogBtn:GetHeight());
+                                        print(UItogBtn:GetLeft());
+                                        print(UItogBtn:GetRight());
+                                        -- UItogBtn:SetWidth(w);
+                                        -- UItogBtn:SetHeight(h);
+                                        -- UItogBtn:SetLeft(l);
+                                        -- UItogBtn:SetRight(r);
                                         print("|cffff0000JOINED LFG|r");
                                     else
                                         LeaveChannelByName("LookingForGroup");
                                         --print("LEFT LFG");
+                                        print("|cffff0000LEFT LFG|r");
                                     end
                                 end);                  
 
@@ -103,12 +87,13 @@ SlashCmdList.FRAMESTK = function()
 end
 
 SLASH_SLFG1 = "/slfg";
-
 SlashCmdList["SLFG"] = function(msg)
     msg = string.upper(msg);
     if (msg == "HELP") then
-        print("-Silence LFG Channel Addon-");
+        print("--Silence LFG Channel Addon--");
         print("'/slfg hide' to hide SLFG");
+        print("'/slfg UL' to unlock the frame");
+        print("'/slfg L' to lock the frame");
         print("'/slfg show' to show SLFG");
         print("'/slfg join' to join LFG");
         print("'/slfg leave' to leave LFG");
@@ -125,16 +110,24 @@ SlashCmdList["SLFG"] = function(msg)
     elseif (msg == "LEAVE") then
         LeaveChannelByName("LookingForGroup");
         print("LEAVE");
-    elseif (msg == "UNLOCK") then
+    elseif (msg == "UL") then
         -- TODO: toggle lock for position of SLFG's main frame
         UItogBtn:SetMovable(true);
         UItogBtn:RegisterForDrag("LeftButton");
         UItogBtn:SetScript("OnDragStart", UItogBtn.StartMoving);
         UItogBtn:SetScript("OnDragStop", UItogBtn.StopMovingOrSizing);
-        print("LOCK");
-    elseif (msg == "LOCK") then
+        UItogBtn:SetUserPlaced(true);
+        print("SLFG FRAME UNLOCKED");
+    elseif (msg == "L") then
         UItogBtn:StopMovingOrSizing();
         UItogBtn:RegisterForDrag();
         UItogBtn:SetMovable(false);
+        print("SLFG FRAME LOCKED");
+    elseif (msg == "Q") then
+        print("W-H-L-R");
+        print(UItogBtn:GetWidth());
+        print(UItogBtn:GetHeight());
+        print(UItogBtn:GetLeft());
+        print(UItogBtn:GetRight());
     end
 end
